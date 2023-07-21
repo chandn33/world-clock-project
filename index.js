61447c6fb8293de328874cbf29af75f8d8656203
chandn33 <misschandni_patel003@hotmail.co.uk>
2023-07-21 09:16:56 +0100

    updated github url

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ setInterval(function () {
   let manchesterTimeElement = manchesterElement.querySelector(".time");
   let manchesterTime = moment().tz("Europe/London");
 
-  manchesterDateElement.innerHTML = manchesterTime.format("dddd Do MMMM YYYY");
+  manchesterDateElement.innerHTML = manchesterTime.format("ddd Do MMMM yyyy");
   manchesterTimeElement.innerHTML = manchesterTime.format(
     "h:mm:ss [<small>]A[</small>]"
   );
@@ -14,7 +14,7 @@ setInterval(function () {
   let veniceTimeElement = veniceElement.querySelector(".time");
   let veniceTime = moment().tz("Europe/Rome");
 
-  veniceDateElement.innerHTML = veniceTime.format("dddd Do MMMM YYYY");
+  veniceDateElement.innerHTML = veniceTime.format("ddd Do MMMM yyyy");
   veniceTimeElement.innerHTML = veniceTime.format(
     "h:mm:ss [<small>]A[</small>]"
   );
@@ -24,7 +24,7 @@ setInterval(function () {
   let newYorkTimeElement = newYorkElement.querySelector(".time");
   let newYorkTime = moment().tz("America/New_York");
 
-  newYorkDateElement.innerHTML = newYorkTime.format("dddd Do MMMM YYYY");
+  newYorkDateElement.innerHTML = newYorkTime.format("ddd Do MMMM yyyy");
   newYorkTimeElement.innerHTML = newYorkTime.format(
     "h:mm:ss [<small>]A[</small>]"
   );
